2ae190a36563cb24f5c73334c27848e66454b340
Alexander <forworkmanit@gmail.com>
2017-02-26 02:37:39 +0300

    working app

--- app/src/main/java/com/example/freeman/test/MapsActivity.java
+++ app/src/main/java/com/example/freeman/test/MapsActivity.java
@@ -1,96 +1,314 @@
 package com.example.freeman.test;
 
-import android.support.v4.app.FragmentActivity;
+import android.Manifest;
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.os.Build;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.vision.text.Text;
-import android.widget.TextView;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
 
+    // Get Class Name
+    private static String TAG = MapsActivity.class.getName();
+    private Context mContext;
+
+    SupportMapFragment mapFragment;
     private GoogleMap mMap;
+    GoogleApiClient mGoogleApiClient;
+    Location mLastLocation;
+    Marker mCurrLocationMarker;
+    LocationRequest mLocationRequest;
+    // How many Geocoder should return our GPSTracker
+    int geocoderMaxResults = 1;
+
     private float zoom;
+    LatLng latlng;
     Double latitude;
     Double longitude;
-    TextView label_latitude;
-    TextView label_longitude;
-    TextView location;
+    String timestamp;
+    String location;
+    String countryName;
+    TextView latitudeView;
+    TextView longitudeView;
+    TextView timestampView;
+    TextView locationView;
+    TextView providerView;
+
+    private LocationManager locationManager;
+    private Marker mMarker;
+
+    StringBuilder sbGPS = new StringBuilder();
+    StringBuilder sbNet = new StringBuilder();
+
+    // The minimum time between updates in milliseconds
+    private static final long MIN_TIME_BW_UPDATES = 1000; // 1 second
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+
+//    public MapsActivity(Context context) {
+//        this.mContext = context;
+//        onResume();
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            checkLocationPermission();
+        }
+
+        latitudeView = (TextView) findViewById(R.id.latitude);
+        longitudeView = (TextView) findViewById(R.id.longitude);
+        timestampView = (TextView) findViewById(R.id.timestamp);
+        locationView = (TextView) findViewById(R.id.location);
+        providerView = (TextView) findViewById(R.id.provider);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        zoom = 16;
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        GPSTracker gpsTracker = new GPSTracker(this);
+//        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+    }
 
-        if (gpsTracker.getIsGPSTrackingEnabled())
-        {
-            latitude = gpsTracker.latitude;
-            String stringLatitude = String.valueOf(latitude);
-            label_latitude = (TextView)findViewById(R.id.latitude);
-            label_latitude.setText(stringLatitude);
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
+    }
 
-            longitude = gpsTracker.longitude;
-            String stringLongitude = String.valueOf(longitude);
-            label_longitude = (TextView)findViewById(R.id.longitude);
-            label_longitude.setText(stringLongitude);
-
-//            String country = gpsTracker.getCountryName(this);
-//            textview = (TextView)findViewById(R.id.fieldCountry);
-//            textview.setText(country);
-
-            String city = gpsTracker.getLocality(this);
-            location = (TextView)findViewById(R.id.city);
-            location.setText(city);
-
-//            String postalCode = gpsTracker.getPostalCode(this);
-//            textview = (TextView)findViewById(R.id.fieldPostalCode);
-//            textview.setText(postalCode);
-//
-//            String addressLine = gpsTracker.getAddressLine(this);
-//            textview = (TextView)findViewById(R.id.fieldAddressLine);
-//            textview.setText(addressLine);
-        }
-        else
-        {
-            // can't get location
-            // GPS or Network is not enabled
-            // Ask user to enable GPS/network in settings
-            gpsTracker.showSettingsAlert();
+    @Override
+    public void onConnected(Bundle bundle) {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(MIN_TIME_BW_UPDATES);
+        mLocationRequest.setFastestInterval(MIN_TIME_BW_UPDATES);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
         }
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    public List<Address> getGeocoderAddress(Context context) {
+        if (mLastLocation != null) {
+
+            Geocoder geocoder = new Geocoder(context, Locale.ENGLISH);
+
+            try {
+                /**
+                 * Geocoder.getFromLocation - Returns an array of Addresses
+                 * that are known to describe the area immediately surrounding the given latitude and longitude.
+                 */
+                List<Address> addresses = geocoder.getFromLocation(latitude, longitude, this.geocoderMaxResults);
+
+                return addresses;
+            } catch (IOException e) {
+                //e.printStackTrace();
+                Log.e(TAG, "Impossible to connect to Geocoder", e);
+            }
+        }
+
+        return null;
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mLastLocation = location;
+        if (mCurrLocationMarker != null) {
+            mCurrLocationMarker.remove();
+        }
+        latitude = location.getLatitude();
+        latitudeView.setText(String.format("%.7f", latitude));
+        longitude = location.getLongitude();
+        longitudeView.setText(String.format("%.7f", longitude));
+        providerView.setText(location.getProvider());
+//        locationView.setText(getLocality(Context context));
+//        timestamp = String.format("%", location.getTime());
+//        timestampView.setText(timestamp);
+
+        //Place current location marker
+        LatLng latLng = new LatLng(latitude, longitude);
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latLng);
+        markerOptions.title("Current Position");
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+        mCurrLocationMarker = mMap.addMarker(markerOptions);
+
+        //move map camera
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(zoom));
+
+        //stop location updates
+        if (mGoogleApiClient != null) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+        }
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        latitude = 53.911954;
+        longitude = 27.593078;
+
+    }
+
+    public boolean checkLocationPermission(){
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Asking user if explanation is needed
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                //Prompt the user once explanation has been shown
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted. Do the
+                    // contacts-related task you need to do.
+                    if (ContextCompat.checkSelfPermission(this,
+                            Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+
+                        if (mGoogleApiClient == null) {
+                            buildGoogleApiClient();
+                        }
+                        mMap.setMyLocationEnabled(true);
+                    }
+                } else {
+                    // Permission denied, Disable the functionality that depends on this permission.
+                    Toast.makeText(this, "permission denied", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other permissions this app might request.
+            // You can add here other case statements according to your requirement.
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        //stop location updates when Activity is no longer active
+        if (mGoogleApiClient != null) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+        }
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        zoom = 14;
-        // Add a marker in Sydney and move the camera
-        LatLng location = new LatLng(latitude, longitude);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location , zoom));
-        mMap.addMarker(new MarkerOptions().position(location).title("You are here"));
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
+        //Initialize Google Play Services
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+                buildGoogleApiClient();
+                mMap.setMyLocationEnabled(true);
+            }
+        }
+        else {
+            buildGoogleApiClient();
+            mMap.setMyLocationEnabled(true);
+        }
     }
 
+    public String getCountryName(Context context) {
+        List<Address> addresses = getGeocoderAddress(context);
+        if (addresses != null && addresses.size() > 0) {
+            Address address = addresses.get(0);
+            String countryName = address.getCountryName();
 
+            return countryName;
+        } else {
+            return null;
+        }
+    }
 
+    public String getLocality(Context context) {
+        List<Address> addresses = getGeocoderAddress(context);
+
+        if (addresses != null && addresses.size() > 0) {
+            Address address = addresses.get(0);
+            String locality = address.getLocality();
+
+            return locality;
+        }
+        else {
+            return null;
+        }
+    }
+
+    public void onClickLocationSettings(View view) {
+        startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+    }
 }
